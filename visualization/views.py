c5aae526c52233f41229df18c64a94fe0c4e5121
Gio <60558512+GiovanniCornejo@users.noreply.github.com>
2024-04-09 18:05:41 -0400

    Stored csv data in views.py

--- visualization/views.py
+++ visualization/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+
+# Store csv data globally
+import csv
+def load_csv_data():
+    with open("data/covid-data-10-16-20.csv", 'r') as csvfile:
+        return [line for line in csv.DictReader(csvfile)]
+data = load_csv_data()
 
 # Create your views here.
 def home(request: HttpRequest) -> HttpResponse:
@@ -8,24 +15,24 @@ def home(request: HttpRequest) -> HttpResponse:
 def us_cases(request: HttpRequest) -> HttpResponse:
     # TODO: Implement us_cases view
     print("TODO: Implement us_cases view")
-    return redirect('home')
+    return render(request, "visualization/us_cases.html")
 
 def avg_cases_and_deaths(request: HttpRequest) -> HttpResponse:
     # TODO: Implement avg_cases_and_deaths view
     print("TODO: Implement avg_cases_and_deaths view")
-    return redirect('home')
+    return render(request, "visualization/avg_cases_and_deaths.html")
 
 def top5_radar(request: HttpRequest) -> HttpResponse:
     # TODO: Implement top5_radar view
     print("TODO: Implement top5_radar view")
-    return redirect('home')
+    return render(request, "visualization/top5_radar.html")
 
 def max_diff(request: HttpRequest) -> HttpResponse:
     # TODO: Implement max_diff view
     print("TODO: Implement max_diff view")
-    return redirect('home')
+    return render(request, "visualization/max_diff.html")
 
 def positive_rates(request: HttpRequest) -> HttpResponse:
     # TODO: Implement positive_rates view
     print("TODO: Implement positive_rates view")
-    return redirect('home')
+    return render(request, "visualization/positive_rates.html")

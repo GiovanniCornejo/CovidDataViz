7ce465f21b21e15ddc5dfca8b564a97233c7dd75
Gio <60558512+GiovanniCornejo@users.noreply.github.com>
2024-04-10 00:08:56 -0400

    Finished avg_cases_and_deaths

--- visualization/urls.py
+++ visualization/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("us_cases", views.us_cases, name="us_cases"),
-    path("avg_cases_and_deaths", views.avg_cases_and_deaths, name="avg_cases_and_death"),
+    path("avg_cases_and_deaths", views.avg_cases_and_deaths, name="avg_cases_and_deaths"),
     path("top5_radar", views.top5_radar, name="top5_radar"),
     path("max_diff", views.max_diff, name="max_diff"),
     path("positive_rates", views.positive_rates, name="positive_rates")
--- visualization/views.py
+++ visualization/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Store csv data globally
 import pandas as pd
+import numpy as np
+
+# Store csv data globally
 covid_df = pd.read_csv("data/covid-data-10-16-20.csv")
+covid_df["month"] = pd.to_datetime(covid_df["date"]).dt.month
 
 # Create your views here.
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'visualization/home.html')
+    return render(request, "visualization/home.html")
 
 def us_cases(request: HttpRequest) -> HttpResponse:
     """
-    Generate a double line graph visualization that plots the total and new number of COVID cases 
+    Generates a double line graph visualization that plots the total and new number of COVID cases 
     in the United States where data is available (no null values) over time.
 
     Return:
@@ -23,18 +26,45 @@ def us_cases(request: HttpRequest) -> HttpResponse:
         (covid_df["iso_code"] == "USA") & covid_df["total_cases"] & covid_df["new_cases_smoothed"]
     ]
 
-    return render(request, "visualization/us_cases.html", 
-        context={ 
-            "labels": us_cases["date"].tolist(), 
-            "total_cases": us_cases["total_cases"].tolist(),
-            "new_cases": us_cases["new_cases_smoothed"].tolist()
-        }
-    )
+    return render(request, "visualization/us_cases.html", { 
+        "labels": us_cases["date"].tolist(), 
+        "total_cases": us_cases["total_cases"].tolist(),
+        "new_cases": us_cases["new_cases_smoothed"].tolist()
+    })
 
 def avg_cases_and_deaths(request: HttpRequest) -> HttpResponse:
-    # TODO: Implement avg_cases_and_deaths view
-    print("TODO: Implement avg_cases_and_deaths view")
-    return render(request, "visualization/avg_cases_and_deaths.html")
+    """
+    Generates a stacked bar graph visualization to show: For which month did each country have the
+    highest on average daily new cases along with how many total deaths happened in this month.
+    Plots the top 5 countries with the most new cases.
+
+    Return:
+    `labels`: The country names.
+    `new_cases`: Average number of daily cases that month.
+    `new_deaths`: Total number of new deaths that month.
+    """    
+    # Filter out non-country locations
+    df = covid_df[(covid_df["location"] != "International") & (covid_df["location"] != "World")]
+
+    monthly_country_data = df.groupby(["location", "month"]).agg(
+        avg_cases=("new_cases", "mean"),
+        new_deaths=("new_deaths", "sum")
+    ).dropna(subset=["avg_cases", "new_deaths"])
+
+    # Filter each country to month with highest average daily cases
+    highest_avg_daily_cases = monthly_country_data.groupby("location")["avg_cases"].idxmax()
+    highest_monthly_country_data = monthly_country_data.loc[highest_avg_daily_cases]
+
+    # Get top 5 countries with highest average daily new cases in one month
+    top5_countries = highest_monthly_country_data["avg_cases"].nlargest(5).index.get_level_values("location")
+    avg_cases = highest_monthly_country_data.loc[top5_countries, "avg_cases"]
+    new_deaths = highest_monthly_country_data.loc[top5_countries, "new_deaths"]
+
+    return render(request, "visualization/avg_cases_and_deaths.html", {
+        "labels": top5_countries.tolist(),
+        "new_cases": avg_cases.tolist(),
+        "new_deaths": new_deaths.tolist()
+    })
 
 def top5_radar(request: HttpRequest) -> HttpResponse:
     # TODO: Implement top5_radar view
